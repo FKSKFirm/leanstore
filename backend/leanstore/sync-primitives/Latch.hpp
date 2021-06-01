b972e3ee0730ddbfb36856a8169d2b2b75f1d69d
Florian Kronberger <mail@floriankronberger.de>
2021-06-01 09:30:54 +0200

    fixed Deadlock problem with scanAsc/Desc in LSM Tree due to multiple BTreeIterators which lock multiple leafs (jumpmu::jump would only free the current iterators leaf and not all leafs)

--- backend/leanstore/sync-primitives/Latch.hpp
+++ backend/leanstore/sync-primitives/Latch.hpp
@@ -173,7 +173,10 @@ struct Guard {
       if (state == GUARD_STATE::OPTIMISTIC) {
          const u64 new_version = version + LATCH_EXCLUSIVE_BIT;
          u64 expected = version;
-         latch->mutex.lock();  // changed from try_lock because of possible retries b/c lots of readers
+         //latch->mutex.lock();  // changed from try_lock because of possible retries b/c lots of readers
+         if (!latch->mutex.try_lock()) {
+            jumpmu::jump();
+         }
          if (!latch->ref().compare_exchange_strong(expected, new_version)) {
             latch->mutex.unlock();
             jumpmu::jump();
